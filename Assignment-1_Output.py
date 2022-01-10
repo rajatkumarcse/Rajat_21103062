a7545c71a0e3aa0019571c8306eb2e3188d9f9ba
rajatkumarcse <96794010+rajatkumarcse@users.noreply.github.com>
2022-01-10 16:44:22 +0530

    Add files via upload

---
+++ Assignment-1_Output.py
@@ -0,0 +1,44 @@
+# Output (Question --> 1)
+
+'''Enter your first number : 10
+Enter your second number : 12
+Enter your third number : 14
+The average of three numbers is :
+12.0'''
+
+
+# Output (Question --> 2)
+
+'''Enter your gross income : 50000
+Enter your number of dependents : 5
+Your income tax is :
+5000.0'''
+
+
+# Output (Question --> 3)
+
+'''Enter your SID : 21103062
+Enter your name : Rajat Kumar
+Enter your gender : M
+Enter your course_name : Computer Science and Engineering
+Enter your CGPA : 9
+(21103062, ' Rajat Kumar', ' M', ' Computer Science and Engineering', 9.0) '''
+
+
+# Output (Question --> 4)
+
+'''Enter marks of student : 94
+Enter marks of student : 95
+Enter marks of student : 98
+Enter marks of student : 97
+Enter marks of student : 96
+[' 94', ' 95', ' 96', ' 97', ' 98']'''
+
+
+# Output (Question --> 5)
+
+# Part (a)
+'''['Red', 'Green', 'White', 'Pink', 'Yellow']'''
+
+# Part (b)
+'''['Red', 'Green', 'White', 'Purple', 'Yellow']'''
